a7c8f852669d4375282f99f2f6508a291417046c
gsparvej <parvejmd4469@gmail.com>
2025-02-27 15:23:48 +0600

    add

--- 27-02-25.sql
+++ 27-02-25.sql
@@ -1,1 +1,10 @@
 select last_name, first_name from employees;
+
+select last_name, job_id, hire_date from employees;
+
+select last_name, job_id,hire_date  from employees
+where hire_date in ('13-JAN-01','28-SEP-05');
+
+select last_name, job_id,hire_date  from employees
+where first_name like 'A%';
+
