6e378546d0fca48378a5f8da2e5a398cc20baf3b
gsparvej <parvejmd4469@gmail.com>
2025-03-03 13:38:10 +0600

    add

--- 03-03-25.sql
+++ 03-03-25.sql
@@ -1,3 +1,8 @@
 select employee_id,hire_date,round(hire_date,'month'),trunc(hire_date,'month')
 from employees
 where hire_date like '%05';
+
+select last_name,hire_date
+from employees
+where hire_date< to_date('May 24, 2004','fxMonth dd, YYYY');
+
