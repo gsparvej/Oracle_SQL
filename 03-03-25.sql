64bc76da5c673f3459436d8e8782dd3265c0669b
gsparvej <parvejmd4469@gmail.com>
2025-03-03 13:56:24 +0600

    add

--- 03-03-25.sql
+++ 03-03-25.sql
@@ -6,3 +6,9 @@ select last_name,hire_date
 from employees
 where hire_date< to_date('May 24, 2004','fxMonth dd, YYYY');
 
+select last_name, job_id, salary,
+case job_id when 'IT_PROG' then 1.10*salary
+when 'ST_CLEARK' then 1.15*salary
+else salary end "salary"
+from employees;
+
