420a2a10217d9d33c8dc1bda1a13081e18fd467b
gsparvej <parvejmd4469@gmail.com>
2025-03-04 16:09:38 +0600

    add

--- multiple table using.sql
+++ multiple table using.sql
@@ -22,3 +22,10 @@ NATURAL JOIN regions
 where salary<6000
 order by 2 desc
  OFFSET 5 ROWS FETCH NEXT 5 ROWS ONLY;
+ 
+ 
+ select e.first_name, d.department_name, l.street_address, l.city, c.country_name
+ from employees e
+ join departments d on e.department_id=d.department_id
+ join Locations l on l.location_id=d.location_id
+ join countries c on l.country_id=c.country_id;
