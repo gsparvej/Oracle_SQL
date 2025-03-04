c47af9bf8f125f2d349db0ac9bb150e5e71ceece
gsparvej <parvejmd4469@gmail.com>
2025-03-04 16:35:33 +0600

    add

--- multiple table using.sql
+++ multiple table using.sql
@@ -35,3 +35,20 @@ order by 2 desc
  where e.salary> 3000
  order by e.salary desc
  OFFSET 5 ROWS FETCH NEXT 5 ROWS ONLY;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ select c.country_name, d.department_name,l.state_province,r.region_name,
+ jo.max_salary,j.start_date
+ from employees e 
+ join countries c on c.country_id_id=l.country_id
+ join departments d on d.department_id=j.department_id
+ join job_history j on j.job_id=jo.job_id
+ join jobs jo on jo.job_id=e.job_id
+ join locations l on l.country_id=c.country_id
+ join regions r on r.region_id=c.region_id;
