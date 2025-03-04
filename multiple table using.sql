4fb5712f7a591e76ed4bc6609ef134376ce13734
gsparvej <parvejmd4469@gmail.com>
2025-03-04 15:17:32 +0600

    add

--- multiple table using.sql
+++ multiple table using.sql
@@ -1,4 +1,24 @@
-select last_name,salary, department_name,manager_id
-from employees NATURAL JOIN departments
+select last_name,salary, department_name,manager_id,country_name,postal_code,
+state_province,max_salary,region_name
+from employees 
+NATURAL JOIN departments 
+NATURAL JOIN countries 
+NATURAL JOIN locations
+NATURAL JOIN jobs
+NATURAL JOIN regions
 where salary>6000
-order by 2 desc;
+order by 2 desc
+fetch FIRST 5 ROWS ONLY;
+
+
+select  distinct salary, department_name,manager_id,country_name,postal_code,
+state_province,max_salary,region_name
+from employees 
+NATURAL JOIN departments 
+NATURAL JOIN countries 
+NATURAL JOIN locations
+NATURAL JOIN jobs
+NATURAL JOIN regions
+where salary>6000
+order by 2 desc
+fetch FIRST 5 ROWS ONLY;
