5790e677f8bb1ca9684551b379d1925321ea19ae
gsparvej <parvejmd4469@gmail.com>
2025-03-10 15:12:43 +0600

    add

--- 10-03-25.sql
+++ 10-03-25.sql
@@ -2,3 +2,20 @@ select  worker.employee_id,worker.last_name emp, manager.last_name mgr,manager.e
 from employees worker join employees manager 
 on (worker.manager_id=manager.employee_id);
 
+
+select* from jobs;
+
+select e.last_name,d.department_id, d.department_name
+from employees e LEFT OUTER JOIN departments d
+   ON e.department_id = d.department_id;
+   
+   select e.last_name,d.department_id, d.department_name
+from employees e RIGHT OUTER JOIN departments d
+   ON e.department_id = d.department_id;
+   
+
+select e.last_name,d.department_id, d.department_name
+from employees e FULL OUTER JOIN departments d
+ON e.department_id = d.department_id;
+   
+   
