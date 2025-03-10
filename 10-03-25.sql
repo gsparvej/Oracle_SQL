c8eae941bf7741f575e9dbf917a6aa6ff64bfd3e
gsparvej <parvejmd4469@gmail.com>
2025-03-10 14:44:13 +0600

    add

--- 10-03-25.sql
+++ 10-03-25.sql
@@ -1,4 +1,4 @@
-select worker.last_name emp, manager.last_name mgr
+select  worker.employee_id,worker.last_name emp, manager.last_name mgr,manager.employee_id
 from employees worker join employees manager 
 on (worker.manager_id=manager.employee_id);
 
