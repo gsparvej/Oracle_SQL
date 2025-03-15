4bc466919a7e6263dd495a0d031f0c8649146902
gsparvej <parvejmd4469@gmail.com>
2025-03-15 14:44:31 +0600

    add

--- 15-03-25.sql
+++ 15-03-25.sql
@@ -3,3 +3,12 @@ select * from emp1287799;
 DELETE from emp1287799
 where eid= 6;
 ROLLBACK;
+COMMIT;
+DELETE from emp1287799
+where department_id= (select department_id
+                    from emp1287799
+                    where department_name='NT');
+                    
+select department_id
+                    from emp1287799
+                    where department_name='NT';
