283bd652408dab24a05d0c5ca6dd2724bd7dc686
gsparvej <parvejmd4469@gmail.com>
2025-03-17 14:11:40 +0600

    add

---
+++ insert subqueries.sql
@@ -0,0 +1,22 @@
+SELECT
+    * FROM emp1287799;
+    
+    
+TRUNCATE TABLE emp1287799;
+    
+    
+INSERT into emp1287799 (eid,ename,salary,hire_date,address,department_id,department_name)
+select emp1287799_eid_seq.nextval,last_name,salary,hire_date,city, DEPARTMENT_ID,DEPARTMENT_NAME
+from hr.EMPLOYEES
+natural join hr.DEPARTMENTS
+natural join hr.LOCATIONS
+where salary > (
+                 select max(salary)
+                 from hr.EMPLOYEES
+                 where job_id like 'ST_CLERK'
+);
+
+
+select max(salary)
+                 from hr.EMPLOYEES
+                 where job_id like 'ST_CLERK';
