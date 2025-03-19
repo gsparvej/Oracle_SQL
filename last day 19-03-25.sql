fb3f026dc05de2d800a499e3e4a9576e9824fa5c
gsparvej <parvejmd4469@gmail.com>
2025-03-19 16:08:00 +0600

    update

--- last day 19-03-25.sql
+++ last day 19-03-25.sql
@@ -28,5 +28,13 @@ INSERT into emp1287799 (eid,ename,salary,hire_date,address, department_id,depart
  from employees
  where salary between 7000 and 8000);
  
+ SELECT* from emp1287799;
+ 
+ INSERT into emp1287799 (eid,ename,salary,hire_date,address, department_id,department_name)
+ values(emp1287799_eid_seq.nextval,'Ratan',28000,'02-oct-24','Dhaka',111,'W');
+ INSERT into emp1287799 (eid,ename,salary,hire_date,address, department_id,department_name)
+ values(emp1287799_eid_seq.nextval,'Raju',28000,'02-feb-23','Dhaka',110,'L');
+ 
+ 
  
  
